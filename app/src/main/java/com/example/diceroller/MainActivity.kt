dcc3d8facb5437aeffef896c470113c8ebcb3adf
kim dillon <kim.alexandra.dillon@gmail.com>
2022-11-29 19:33:40 -0800

    shows binding of roll button add on clicklistener add toast to show button was clicked

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.diceroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
+        binding.rollbutton.text = "FUN TIMES"
+        binding.rollbutton.setOnClickListener{
+            Toast.makeText(this,"button clicked", Toast.LENGTH_SHORT).show()
+        }
         setContentView(view)
+
     }
 }
